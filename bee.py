2014e969c88cc664b54a0030152329b9f4791710
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2022-04-05 16:34:49 +0530

    Update bee.py

--- bee.py
+++ bee.py
@@ -1,9 +1,10 @@
-bee_script = open("bee_script.txt", "rt")
-shrek_script = open("shrek_script.txt", "r")
-shrek = shrek_script.read()
-fixed = open("fixed.txt", "wt")
-for line in bee_script:
-    fixed.write(line.replace('bee', shrek))
-bee_script.close()
-shrek_script.close()
-fixed.close()
+with open("shrek_script.txt", "r") as shrek_script:
+    shrek_script = shrek_script.read()
+
+with open("bee_script.txt", "r") as bee_script:
+    bee_lines = bee_script.read().splitlines()
+    with open("fixed.txt", "w") as fixed:
+        for line in bee_lines:
+            line = line.replace("bee", shrek_script)
+            fixed.write(shrek_script + "\n")
+            
